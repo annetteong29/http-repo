261994469c20ddd1225fe78ab63dbc5a5fb18252
ALEX Solutions <alexsolutions@annette-ong.local>
2019-07-22 16:45:04 +1000

    Deleted element

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -10,7 +10,7 @@ class FullPost extends Component {
 
     componentDidUpdate() {
         if (this.props.id) {
-            if (!this.state.loadedPost || this.state.loadedPost && (this.state.loadedPost.id !== this.props.id)) {
+            if (!this.state.loadedPost || (this.state.loadedPost && (this.state.loadedPost.id !== this.props.id))) {
                 axios.get('http://jsonplaceholder.typicode.com/posts/' + this.props.id)
                 .then(response => {
                     // console.log(response);
@@ -21,6 +21,13 @@ class FullPost extends Component {
                 });
             }
         }
+    }
+
+    deletePostHandler = () => {
+        axios.delete('http://jsonplaceholder.typicode.com/posts/' + this.props.id)
+        .then(response => {
+                console.log(response);
+            });
     }
 
     render () {
@@ -34,7 +41,9 @@ class FullPost extends Component {
                     <h1>{this.state.loadedPost.title}</h1>
                     <p>{this.state.loadedPost.body}</p>
                     <div className="Edit">
-                        <button className="Delete">Delete</button>
+                        <button 
+                            onClick={this.deletePostHandler} 
+                            className="Delete">Delete</button>
                     </div>
                 </div>
             );

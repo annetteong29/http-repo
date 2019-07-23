44508be95b393f8256b490bc9a258fb20777e7f5
ALEX Solutions <alexsolutions@annette-ong.local>
2019-07-23 15:32:21 +1000

    Redirect requests + conditionally

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
 // import axios from '../../axios'; 
-import { Route, NavLink, Switch } from 'react-router-dom';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from '../../containers/Blog/Posts/Posts';
@@ -42,6 +42,8 @@ class Blog extends Component {
                 <Switch>
                     <Route path="/new-post" component={NewPost} />
                     <Route path="/posts" component={Posts} />
+                    {/* <Route path="/" component={Posts} /> */}
+                    <Redirect from="/" to="/posts" />
                 </Switch>   
             </div>
         );
--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import Spinner from '../../../components/UI/Spinner/Spinner';
+// import Spinner from '../../../components/UI/Spinner/Spinner';
 
 import './FullPost.css';
 
